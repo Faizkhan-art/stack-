74731c42e986c786a8ba03bca8cd7a572e0ba2cc
Faiz Khan <yahyahkhan@gmail.com>
2026-09-17 23:33:54 +0530

    update

---
+++ stackmethod.py
@@ -0,0 +1,6 @@
+stack=[45,25,87,c]
+
+stack.append(10)   # Push - element add
+stack.pop()        # Pop - last element remove
+stack[-1]          # Peek - top element
+len(stack)         # Stack size
