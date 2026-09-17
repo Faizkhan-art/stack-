f0e60e82a1fe0e561c6d71618dcc598c9b388f36
Faiz Khan <yahyahkhan@gmail.com>
2026-09-17 23:17:34 +0530

    stack

---
+++ stack.py
@@ -0,0 +1,14 @@
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+# Pop
+x = stack.pop()
+print("Removed:", x)
+
+print("Stack after pop:", stack)
